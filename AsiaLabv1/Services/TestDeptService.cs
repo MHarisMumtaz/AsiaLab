267580ddb24df68f07759bcda1688ec01430e51c
MHarisMumtaz <harismumtaz.pafkiet@hotmail.com>
2016-05-08 18:53:37 +0500

    added test dept service

---
+++ AsiaLabv1/Services/TestDeptService.cs
@@ -0,0 +1,23 @@
+﻿using AsiaLabv1.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AsiaLabv1.Services
+{
+    public class TestDeptService
+    {
+        Repository<TestDepartment> _TestDeptRepository = new Repository<TestDepartment>();
+
+        public void Add(TestDepartment TestDept)
+        {
+            _TestDeptRepository.Insert(TestDept);
+        }
+
+        public List<TestDepartment> GetAllDept()
+        {
+            return _TestDeptRepository.GetAll();
+        }
+    }
+}
