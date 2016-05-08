ad559fd74897432bac05db32525156a0eb5b112f
Humamkhan <humamkhan92@live.com>
2016-05-08 19:54:29 +0500

    added new tab in test management

--- AsiaLabv1/Models/TestManagementModel.cs
+++ AsiaLabv1/Models/TestManagementModel.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace AsiaLabv1.Models
 {
-    public class TestManagementModel
+    public class TestManagementModel 
     {
         public int deptId { get; set; }
         public string deptName { get; set; }
         public List<SelectListItem> departments { get; set; }
         public string testCategoryName { get; set; }
         public int testCategoryId { get; set; }
+        public bool IsNewDepartment { get; set; }
+        
+        [Display(Name="Test Category Code")]
+        public string CatgoryCode { get; set; }
 
         public TestManagementModel()
         {
