f0f8934fda86c5adb56b7011d515fc2b07eebbb0
AZ94 <syedz74@gmail.com>
2016-05-13 04:39:10 +0500

    New Service

---
+++ AsiaLabv1/Services/TestSubCategoryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AsiaLabv1.Repositories;
+
+namespace AsiaLabv1.Services
+{
+    public class TestSubCategoryService
+    {
+        Repository<TestSubcategory> _TestSubCatgeroryRepository = new Repository<TestSubcategory>();
+
+        public void Add(TestSubcategory TestCatg)
+        {
+            _TestSubCatgeroryRepository.Insert(TestCatg);
+        }
+
+    }
+}
