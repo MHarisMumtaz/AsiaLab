898319015e4b4f5af015ceafd3eb237154031e74
MHarisMumtaz <harismumtaz.pafkiet@hotmail.com>
2016-05-14 00:09:30 +0500

    no message

---
+++ AsiaLabv1/Services/TestSubCategoryService.cs
@@ -0,0 +1,15 @@
+﻿using AsiaLabv1.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AsiaLabv1.Services
+{
+    public class TestSubCategoryService
+    {
+        Repository<TestSubcategory> _TestSubCatRepository = new Repository<TestSubcategory>();
+
+        public void 
+    }
+}
