ee0894edcf6e6302a029e0529a56b169cf651cbe
MHarisMumtaz <harismumtaz.pafkiet@hotmail.com>
2016-05-18 22:40:12 +0500

    added patienttest service, added method on receiptionistcontroller, modified registerview fields

--- AsiaLabv1/Controllers/ReciptionistController.cs
+++ AsiaLabv1/Controllers/ReciptionistController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AsiaLabv1.Models;
+using AsiaLabv1.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +10,62 @@ namespace AsiaLabv1.Controllers
 {
     public class ReciptionistController : Controller
     {
-        //
-        // GET: /Reciptionist/
+        PatientService PatientServices = new PatientService();
+        PatientTestService PatientTestService = new PatientTestService();
+        GenderService GenderServices = new GenderService();
+        TestDeptService TestDeptServices = new TestDeptService();
 
-        public ActionResult Index()
+        public ActionResult RegisterPatient()
         {
-            return View();
+            var model = new PatientModel();
+            var Genders = GenderServices.GetAll();
+            foreach (var item in Genders)
+            {
+                model.Genders.Add(new SelectListItem
+                {
+                    Text = item.GenderDescription,
+                    Value = item.Id.ToString()
+                });
+            }
+            var allDept = TestDeptServices.GetAllDept();
+            foreach (var Dept in allDept)
+            {
+                model.Departments.Add(new SelectListItem
+                {
+                    Value = Dept.Id.ToString(),
+                    Text = Dept.DepartmentName
+                });
+            }
+
+            return View("RegisterPatient", model);
         }
 
-        public ActionResult AcceptTest()
+        [HttpPost]
+        public ActionResult GetSubCategory(int Id)
         {
+            return Json("asd", JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult AddPatient(PatientModel model)
+        {
+            //var model=new PatientModel(){
+            //    BranchId=1,
+            //    Name="firstTestPatient",
+            //    Email="Testpatient@gmail.com",
+            //    GenderId=1,
+            //    DateofBirth=DateTime.Now,
+            //    PhoneNumber="987987697",
+            //    ReferredId=-1
+            //};
+            PatientServices.Add(model);
+
+            foreach (var TestId in model.PatientTestIds)
+            {
+                PatientTestService.Add(new PatientTest
+                {
+                    PatientId = model.Id,
+                    TestSubcategoryId = TestId
+                });
+            }
             return View();
         }
 
